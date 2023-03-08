11a01b8aced675e3735be74e17cd68892377b314
APZhukov <a.zhukov1995@gmail.com>
2023-03-08 20:25:20 +0600

    8 ДЗ

---
+++ Lesson4/Lesson4/Program.cs
@@ -0,0 +1,31 @@
+﻿//Напишите цикл, который принимает на вход два числа (A и B) и возводит число A в натуральную степень B.
+
+//Console.Write("Введите число А: ");
+//int A = Convert.ToInt32(Console.ReadLine());
+
+//Console.Write("Введите число B: ");
+//int B = Convert.ToInt32(Console.ReadLine());
+
+//double result = Math.Pow(A, B);
+
+//Console.WriteLine("Результат = " + result);
+
+//Напишите программу, которая принимает на вход число и выдаёт сумму цифр в числе.
+
+//Console.Write("Введите число: ");
+//int number = Convert.ToInt32(Console.ReadLine());
+
+//int sum = number % 10;
+
+//while (number != 0)
+//{
+//    int x = number / 10 % 10;
+//    sum += x;
+//    number /= 10;
+//}
+
+//Console.WriteLine(sum);
+
+
+//Напишите программу, которая задаёт массив из 8 элементов и выводит их на экран.
+
